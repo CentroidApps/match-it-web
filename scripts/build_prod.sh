bda50ddfba800d12569d0ab4c0c1aee2a1ae2bc0
CentroidApps <centroidapps@gmail.com>
2024-06-02 17:56:37 +0530

    build script path changes remove unwanted code

--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -22,7 +22,6 @@ destination_dir="docs"
 rm -rf "$source_dir"
 
 ng build --configuration production --base-href /match-it-web/
-#ng build --configuration production --base-href https://centroidapps.github.io/match-it-web/
 
 # Check if the source directory exists
 if [ ! -d "$source_dir" ]; then
